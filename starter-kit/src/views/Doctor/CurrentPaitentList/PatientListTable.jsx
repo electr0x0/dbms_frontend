01afe62e3a81a38e207a73b7ab69c7b968c7d031
itszaman7 <mantaka@protonmail.com>
2024-06-14 12:14:08 +0600

    working patient list

--- starter-kit/src/views/Doctor/CurrentPaitentList/PatientListTable.jsx
+++ starter-kit/src/views/Doctor/CurrentPaitentList/PatientListTable.jsx
@@ -1,153 +1,313 @@
 'use client'
 
+// React Imports
 import React, { useEffect, useState, useMemo } from 'react'
 
+// Next Imports
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+
+// MUI Imports
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import Chip from '@mui/material/Chip'
+import Checkbox from '@mui/material/Checkbox'
+import IconButton from '@mui/material/IconButton'
+import { styled } from '@mui/material/styles'
+import TablePagination from '@mui/material/TablePagination'
+import MenuItem from '@mui/material/MenuItem'
+
+// Third-party Imports
+import classnames from 'classnames'
+import { rankItem } from '@tanstack/match-sorter-utils'
 import {
   createColumnHelper,
-  useReactTable,
+  flexRender,
   getCoreRowModel,
-  getSortedRowModel,
+  useReactTable,
   getFilteredRowModel,
-  getPaginationRowModel
+  getFacetedRowModel,
+  getFacetedUniqueValues,
+  getFacetedMinMaxValues,
+  getPaginationRowModel,
+  getSortedRowModel
 } from '@tanstack/react-table'
-import {
-  Card,
-  CardHeader,
-  CardContent,
-  CardActions,
-  Typography,
-  Button,
-  TablePagination,
-  Checkbox,
-  MenuItem
-} from '@mui/material'
 
+// Component Imports
+import TableFilters from '../../../views/Doctor/CurrentPaitentList/PatientTableFilter'
+import AddPatientDrawer from './AddPatientDrawer'
+import OptionMenu from '@core/components/option-menu'
 import TablePaginationComponent from '@components/TablePaginationComponent'
-import PatientTableFilter from './PatientTableFilter'
 import CustomTextField from '@core/components/mui/TextField'
-import { getInitials } from '@/utils/getInitials'
 import CustomAvatar from '@core/components/mui/Avatar'
+
+// Util Imports
+import { getInitials } from '@/utils/getInitials'
+import { getLocalizedUrl } from '@/utils/i18n'
+
+// Style Imports
+import tableStyles from '@core/styles/table.module.css'
+
+// Styled Components
+const Icon = styled('i')({})
+
+const fuzzyFilter = (row, columnId, value, addMeta) => {
+  const itemRank = rankItem(row.getValue(columnId), value)
+
+  addMeta({ itemRank })
+
+  return itemRank.passed
+}
+
+const DebouncedInput = ({ value: initialValue, onChange, debounce = 500, ...props }) => {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+  }, [value, debounce, onChange])
+
+  return <CustomTextField {...props} value={value} onChange={e => setValue(e.target.value)} />
+}
+
+const patientStatusObj = {
+  active: 'success',
+  inactive: 'secondary'
+}
 
 const columnHelper = createColumnHelper()
 
 const PatientListTable = ({ tableData }) => {
-  const [data, setData] = useState(tableData || [])
-  const [globalFilter, setGlobalFilter] = useState('')
+  const [addPatientOpen, setAddPatientOpen] = useState(false)
   const [rowSelection, setRowSelection] = useState({})
+  const [data, setData] = useState(tableData)
+  const [globalFilter, setGlobalFilter] = useState('')
+
+  const { lang: locale } = useParams()
 
   const columns = useMemo(
     () => [
+      {
+        id: 'select',
+        header: ({ table }) => (
+          <Checkbox
+            {...{
+              checked: table.getIsAllRowsSelected(),
+              indeterminate: table.getIsSomeRowsSelected(),
+              onChange: table.getToggleAllRowsSelectedHandler()
+            }}
+          />
+        ),
+        cell: ({ row }) => (
+          <Checkbox
+            {...{
+              checked: row.getIsSelected(),
+              disabled: !row.getCanSelect(),
+              indeterminate: row.getIsSomeSelected(),
+              onChange: row.getToggleSelectedHandler()
+            }}
+          />
+        )
+      },
       columnHelper.accessor('fullName', {
-        header: 'Name',
-        cell: info => info.row.original.fullName
-      }),
-      columnHelper.accessor('age', {
-        header: 'Age',
-        cell: info => info.row.original.age
+        header: 'Patient',
+        cell: ({ row }) => (
+          <div className='flex items-center gap-4'>
+            <CustomAvatar size={34}>{getInitials(row.original.fullName)}</CustomAvatar>
+            <div className='flex flex-col'>
+              <Typography color='text.primary' className='font-medium'>
+                {row.original.fullName}
+              </Typography>
+              <Typography variant='body2'>{row.original.age} years old</Typography>
+            </div>
+          </div>
+        )
       }),
       columnHelper.accessor('gender', {
         header: 'Gender',
-        cell: info => info.row.original.gender
-      }),
-      columnHelper.accessor('contact', {
-        header: 'Contact',
-        cell: info => info.row.original.contact
-      }),
-      columnHelper.accessor('address', {
-        header: 'Address',
-        cell: info => info.row.original.address
+        cell: ({ row }) => (
+          <Typography className='capitalize' color='text.primary'>
+            {row.original.gender}
+          </Typography>
+        )
       }),
       columnHelper.accessor('diagnosis', {
         header: 'Diagnosis',
-        cell: info => info.row.original.diagnosis
+        cell: ({ row }) => (
+          <Typography className='capitalize' color='text.primary'>
+            {row.original.diagnosis}
+          </Typography>
+        )
       }),
       columnHelper.accessor('doctor', {
         header: 'Doctor',
-        cell: info => info.row.original.doctor
+        cell: ({ row }) => (
+          <Typography className='capitalize' color='text.primary'>
+            {row.original.doctor}
+          </Typography>
+        )
       }),
       columnHelper.accessor('status', {
         header: 'Status',
-        cell: info => info.row.original.status
+        cell: ({ row }) => (
+          <div className='flex items-center gap-3'>
+            <Chip
+              variant='tonal'
+              className='capitalize'
+              label={row.original.status}
+              color={patientStatusObj[row.original.status]}
+              size='small'
+            />
+          </div>
+        )
       }),
-      columnHelper.accessor('admissionDate', {
-        header: 'Admission Date',
-        cell: info => info.row.original.admissionDate
+      columnHelper.accessor('action', {
+        header: 'Action',
+        cell: ({ row }) => (
+          <div className='flex items-center'>
+            <IconButton>
+              <i className='tabler-trash text-[22px] text-textSecondary' />
+            </IconButton>
+            <IconButton>
+              {/* <Link href={getLocalizedUrl(`apps/patient/view/${row.original.id}`, locale)} className='flex'>
+                <i className='tabler-eye text-[22px] text-textSecondary' />
+              </Link> */}
+            </IconButton>
+            <OptionMenu
+              iconClassName='text-[22px] text-textSecondary'
+              options={[
+                {
+                  text: 'Download',
+                  icon: 'tabler-download text-[22px]',
+                  menuItemProps: { className: 'flex items-center gap-2 text-textSecondary' }
+                },
+                {
+                  text: 'Edit',
+                  icon: 'tabler-edit text-[22px]',
+                  menuItemProps: { className: 'flex items-center gap-2 text-textSecondary' }
+                }
+              ]}
+            />
+          </div>
+        ),
+        enableSorting: false
       })
     ],
-    []
+    [locale]
   )
 
   const table = useReactTable({
     data,
     columns,
-    state: {
-      rowSelection,
-      globalFilter
-    },
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
+    filterFns: { fuzzy: fuzzyFilter },
+    state: { rowSelection, globalFilter },
+    initialState: { pagination: { pageSize: 10 } },
+    enableRowSelection: true,
+    globalFilterFn: fuzzyFilter,
     onRowSelectionChange: setRowSelection,
-    onGlobalFilterChange: setGlobalFilter
+    getCoreRowModel: getCoreRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getFacetedMinMaxValues: getFacetedMinMaxValues()
   })
 
   return (
-    <Card>
-      <CardHeader title='Patient List' />
-      <PatientTableFilter setData={setData} tableData={tableData} />
-      <CardContent>
-        <table>
-          <thead>
-            {table.getHeaderGroups().map(headerGroup => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map(header => (
-                  <th key={header.id}>
-                    {header.isPlaceholder ? null : (
-                      <div onClick={header.column.getToggleSortingHandler()}>
-                        {typeof header.column.columnDef.header === 'function'
-                          ? header.column.columnDef.header()
-                          : header.column.columnDef.header}
-                        {header.column.getIsSorted() ? (header.column.getIsSorted() === 'desc' ? ' 🔽' : ' 🔼') : ''}
-                      </div>
-                    )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.length > 0 ? (
-              table.getRowModel().rows.map(row => (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map(cell => (
-                    <td key={cell.id}>{cell.value}</td>
+    <>
+      <Card>
+        <CardHeader title='Filters' className='pbe-4' />
+        <TableFilters setData={setData} tableData={tableData} />
+        <div className='flex justify-between flex-col items-start md:flex-row md:items-center p-6 border-bs gap-4'>
+          <CustomTextField
+            select
+            value={table.getState().pagination.pageSize}
+            onChange={e => table.setPageSize(Number(e.target.value))}
+            className='is-[70px]'
+          >
+            <MenuItem value='10'>10</MenuItem>
+            <MenuItem value='25'>25</MenuItem>
+            <MenuItem value='50'>50</MenuItem>
+          </CustomTextField>
+          <div className='flex flex-col sm:flex-row is-full sm:is-auto items-start sm:items-center gap-4'>
+            <DebouncedInput
+              value={globalFilter ?? ''}
+              onChange={value => setGlobalFilter(String(value))}
+              placeholder='Search Patient'
+              className='is-full sm:is-auto'
+            />
+            <Button
+              color='secondary'
+              variant='tonal'
+              startIcon={<i className='tabler-upload' />}
+              className='is-full sm:is-auto'
+            >
+              Export
+            </Button>
+            <Button
+              variant='contained'
+              startIcon={<i className='tabler-plus' />}
+              onClick={() => setAddPatientOpen(!addPatientOpen)}
+              className='is-full sm:is-auto'
+            >
+              Add New Patient
+            </Button>
+          </div>
+        </div>
+        <div className='overflow-x-auto'>
+          <table className={tableStyles.table}>
+            <thead>
+              {table.getHeaderGroups().map(headerGroup => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map(header => (
+                    <th key={header.id} colSpan={header.colSpan}>
+                      {header.isPlaceholder ? null : (
+                        <div
+                          {...{
+                            className: classnames({
+                              [tableStyles['cursor-pointer']]: header.column.getCanSort()
+                            }),
+                            onClick: header.column.getToggleSortingHandler()
+                          }}
+                        >
+                          {flexRender(header.column.columnDef.header, header.getContext())}
+                          {{
+                            asc: <i className='tabler-chevron-up' />,
+                            desc: <i className='tabler-chevron-down' />
+                          }[header.column.getIsSorted()] ?? null}
+                        </div>
+                      )}
+                    </th>
                   ))}
                 </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={columns.length} className='text-center'>
-                  No data available
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </CardContent>
-      <TablePagination
-        component={() => <TablePaginationComponent table={table} />}
-        count={table.getFilteredRowModel().rows.length}
-        rowsPerPage={table.getState().pagination.pageSize}
-        page={table.getState().pagination.pageIndex}
-        onPageChange={(_, page) => {
-          table.setPageIndex(page)
-        }}
-      />
-      <CardActions>
-        <Button variant='contained'>Add New Patient</Button>
-      </CardActions>
-    </Card>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map(row => (
+                <tr key={row.id} className={classnames({ [tableStyles['is-selected']]: row.getIsSelected() })}>
+                  {row.getVisibleCells().map(cell => (
+                    <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <TablePaginationComponent table={table} />
+      </Card>
+      <AddPatientDrawer open={addPatientOpen} handleClose={() => setAddPatientOpen(false)} />
+    </>
   )
 }
 
